dfdc58737b24890e70ff743108974c33ed357417
uptodawn <uptodawn@foxmail.com>
2025-05-22 17:05:40 +0000

    disable default rules

--- config.h
+++ config.h
@@ -27,8 +27,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp_EXAMPLE",     NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox_EXAMPLE",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
